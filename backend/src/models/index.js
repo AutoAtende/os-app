c738355effc5f697e8e0d55373139caae8915cab
Lucas Saud <90074068+LucasSaud@users.noreply.github.com>
2025-02-02 18:22:13 -0300

    fx

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,3 +1,5 @@
+const Sequelize = require('sequelize');
+const config = require('../config/database');
 const User = require('./User');
 const Equipment = require('./Equipment');
 const ServiceOrder = require('./ServiceOrder');
@@ -6,32 +8,31 @@ const Notification = require('./Notification');
 const File = require('./File');
 const Report = require('./Report');
 
+// Inicializa a conexão com o banco de dados
+const env = process.env.NODE_ENV || 'development';
+const sequelize = new Sequelize(config[env]);
+
+// Define os modelos
 const models = {
-  User: User,
-  Equipment: Equipment,
-  ServiceOrder: ServiceOrder,
-  MaintenanceHistory: MaintenanceHistory,
-  Notification: Notification,
-  File: File,
-  Report: Report
+  User,
+  Equipment,
+  ServiceOrder,
+  MaintenanceHistory,
+  Notification,
+  File,
+  Report,
+  sequelize,
+  Sequelize
 };
 
 // Inicializa os modelos
-Object.values(models).forEach(model => {
-  if (model.init) {
-    model.init(sequelize);
-  }
-});
+Object.values(models)
+  .filter(model => typeof model.init === 'function')
+  .forEach(model => model.init(sequelize));
 
 // Define as associações
-Object.values(models).forEach(model => {
-  if (model.associate) {
-    model.associate(models);
-  }
-});
+Object.values(models)
+  .filter(model => typeof model.associate === 'function')
+  .forEach(model => model.associate(models));
 
-module.exports = {
-  sequelize,
-  Sequelize,
-  ...models
-};
+module.exports = models;
