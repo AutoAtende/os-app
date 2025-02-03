f4db5a41a79dd5f4e9e100ef45da4dfa71cbe0cc
Lucas Saud <90074068+LucasSaud@users.noreply.github.com>
2025-02-02 23:24:14 -0300

    Update 20250203021507-admin-user.js

--- backend/src/database/seeders/20250203021507-admin-user.js
+++ backend/src/database/seeders/20250203021507-admin-user.js
@@ -3,10 +3,10 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('User', [{
+    return queryInterface.bulkInsert('users', [{
       name: 'Administrador',
       email: 'admin@admin.com',
-      password: await bcrypt.hash('123456', 8),
+      password_hash: await bcrypt.hash('123456', 8),  // Mudamos de password para password_hash
       role: 'admin',
       active: true,
       created_at: new Date(),
@@ -15,6 +15,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null, {});
+    return queryInterface.bulkDelete('users', null, {});
   }
 };
